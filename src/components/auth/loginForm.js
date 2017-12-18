754fd003539d011ef1be1092da26c9a2097c2de3
alankar <alankar.anand@wonderpoint.com>
2017-12-19 03:08:44 +0530

    css changes

--- src/components/auth/loginForm.js
+++ src/components/auth/loginForm.js
@@ -94,6 +94,8 @@ class LoginForm extends Component {
             <Link to="/signup" className="pull-left">I don't have an account ?</Link>
             <Link to="/reset-password" className="pull-right">Forgot password ?</Link>
           </div>
+          <div className="clearfix">
+          </div>
         </form>
         <div className="form-signin demo-link">
           <Link to="/demo">Learn More</Link>
